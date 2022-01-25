4ecfd14836146a865dab1b56d749199c120d5e65
apple <apple@APPLECE0B>
2022-01-25 16:23:06 +0500

    Add project files.

---
+++ WebApplication5/Controllers/Names.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WebApplication5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Names : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public Names(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select personName from firstapi.name";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("AuthenticationDbContextContextConnection");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon)) 
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+
+                }
+
+
+            }
+
+            return new JsonResult(table);
+
+
+        }
+
+    }
+}
---
+++ WebApplication5/Model/Name.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication5.Model
+{
+    public class Name
+    {
+        public int personId { get; set; }
+        public string personName { get; set; }
+    }
+}
